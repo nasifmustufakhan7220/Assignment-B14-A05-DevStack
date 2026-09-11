de4a6675a4e879137a53f34d1b59a1a803e7fe58
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-09-11 19:21:34 +0600

    Lift up state set up done now on add to cart btn click data go to its parent component

--- src/Components/ExploreTechnologies/ExploreTechnologies.tsx
+++ src/Components/ExploreTechnologies/ExploreTechnologies.tsx
@@ -1,15 +1,50 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { ITechnologyType } from "../../Type/Type";
+import ExploreTechnologyCard from "../ExploreTechnologyCard/ExploreTechnologyCard";
 
 interface IExploreTechnologiesProps{
     exploreTechnologyPromise: Promise<ITechnologyType[]>
 }
 const ExploreTechnologies = ({exploreTechnologyPromise}:IExploreTechnologiesProps) => {
     const technologies = use(exploreTechnologyPromise);
-    console.log(technologies);
+
+    const [stack, setStack] = useState<ITechnologyType[]>([]);
+
+    const handelStack = (t:ITechnologyType):void=>{
+        const newStack = [...stack, t];
+        setStack(newStack);
+    }
     return (
-        <div className="max-w-350 mx-auto">
-            
+        <div className="max-w-330 mx-auto grid grid-cols-12 gap-4">
+            {/* 70% */}
+            <div className="col-span-9 grid grid-cols-1 md:grid-cols-3 gap-4">
+                {
+                    technologies.map(technology => <ExploreTechnologyCard key={technology.id} technology={technology} handelStack={handelStack} />)
+                }
+            </div>
+            {/* 30% */}
+            <div className="col-span-3 ">
+                <div className="grid grid-cols-1 w-full rounded-2xl border border-gray-100 bg-white p-5 shadow-sm">
+
+
+                    {/* Title */}
+                    <h2 className="text-[16px] font-semibold text-gray-900">
+                        Your Stack
+                    </h2>
+
+                    {/* Subtitle */}
+                    <p className="mt-1 text-[13px] text-gray-400">
+                        No technologies selected yet.
+                    </p>
+
+                    {/* Empty Stack Box */}
+                    <div className="mt-4 flex h-16.25 items-center justify-center rounded-xl border border-dashed border-gray-200">
+                        <p className="text-[13px] text-gray-400">
+                        Your stack is empty.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Components/ExploreTechnologyCard/ExploreTechnologyCard.tsx
@@ -0,0 +1,72 @@
+import { FaStar } from "react-icons/fa";
+import type { ITechnologyType } from "../../Type/Type";
+
+interface ITechnologyProps{
+    technology: ITechnologyType;
+    handelStack: (t:ITechnologyType) => void
+}
+
+const ExploreTechnologyCard = ({technology, handelStack}:ITechnologyProps) => {
+    const {badge, category, description, difficulty, icon, name, rating} = technology;
+
+    const handelAddToCart = ()=>{
+        handelStack(technology);
+    }
+    return (
+    <div className="w-full rounded-2xl border border-gray-200 bg-white p-5 shadow-sm">
+
+      {/* Icon + Popular */}
+      <div className="flex items-center justify-between">
+        
+        <img src={icon} alt={name} className="w-12 h-12 object-contain"/>
+        
+
+        <span className="rounded-full bg-blue-50 px-3 py-1 text-xs font-medium text-blue-500">
+          {badge}
+        </span>
+      </div>
+
+      {/* Title */}
+      <h3 className="mt-5 text-xl font-bold text-gray-900">
+        {name}
+      </h3>
+
+      {/* Description */}
+      <p className="mt-2 text-sm leading-5 text-gray-500">
+        {description}
+      </p>
+
+      {/* Divider */}
+      <div className="my-4 border-t border-gray-100"></div>
+
+      {/* Tags + Rating */}
+      <div className="flex items-center justify-between">
+
+        <span className="rounded-md bg-gray-100 px-2 py-1 text-xs text-gray-600">
+          {category}
+        </span>
+
+        <span className="text-xs text-gray-500">
+          {difficulty}
+        </span>
+
+        <div className="flex items-center gap-1">
+          <FaStar className="text-xs text-yellow-400" />
+          <span className="text-sm font-medium text-gray-600">
+            {rating}
+          </span>
+        </div>
+
+      </div>
+
+      {/* Add to Stack Button */}
+      <button onClick={handelAddToCart}
+        className="mt-4 w-full rounded-lg bg-gray-950 py-2.5 text-sm font-medium text-white transition hover:bg-gray-800">
+        Add to Stack
+      </button>
+
+    </div>
+  );
+};
+
+export default ExploreTechnologyCard;
